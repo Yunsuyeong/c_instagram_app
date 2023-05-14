599bda78ae99a1e22e4cc05f915832a4e80a9d33
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-05-14 22:52:29 +0900

    2023/05/14(Page Navigation, Welcome Page)

---
+++ navigators/LoggedOutNav.js
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import CreateAccount from "../screens/CreateAccount";
+import Login from "../screens/Login";
+import Welcome from "../screens/Welcome";
+
+const Stack = createNativeStackNavigator();
+
+const LoggedOutNav = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{ presentation: "card" }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        options={{
+          headerShown: false,
+        }}
+        component={Welcome}
+      />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+    </Stack.Navigator>
+  );
+};
+
+export default LoggedOutNav;
---
+++ screens/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Login</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <View>
+          <Text>Go to Create Account</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Login;
